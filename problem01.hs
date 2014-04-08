b264b0f6b3f49ef6de8ad5921db1267f94aa8de5
Sefa Kilic <sefakilic@gmail.com>
2014-04-07 23:24:18 -0400

    first problem

---
+++ problem01.hs
@@ -0,0 +1,13 @@
+-- (*) Find the last element of a list.
+
+-- (Note that the Lisp transcription of this problem is incorrect.)
+
+-- Example in Haskell:
+
+-- Prelude> myLast [1,2,3,4]
+-- 4
+-- Prelude> myLast ['x','y','z']
+-- 'z'
+
+myLast (x:[]) = x
+myLast (x:xs) = myLast xs
